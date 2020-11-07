adef2c21bd3030bda1410266e1e586c68592c22f
Tom Milostny <tommilostny@live.com>
2020-11-07 15:30:00 +0100

    Added ISE,ISIM files

---
+++ inp-proj1/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -o "/home/tommilostny/FITkit-svn/apps/INP/inp-proj1/testbench_isim_beh.exe" -prj "/home/tommilostny/FITkit-svn/apps/INP/inp-proj1/testbench_beh.prj" "work.testbench" 
