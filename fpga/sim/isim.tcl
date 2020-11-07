787db7b1e2c5479405b181753ef9a1ee85b15a7d
Tom Milostny <tommilostny@live.com>
2020-11-07 19:34:06 +0100

    Implemented Vigenere

--- fpga/sim/isim.tcl
+++ fpga/sim/isim.tcl
@@ -8,9 +8,16 @@ proc isim_script {} {
    add_wave_label "-radix ascii" "CODE" /testbench/tb_code
 
    add_divider "Vigenere Inner Signals"
-   add_wave_label "" "state" /testbench/uut/state
    # sem doplnte vase vnitrni signaly. chcete-li v diagramu zobrazit desitkove
    # cislo, vlozte do prvnich uvozovek: -radix dec
+
+   add_wave_label "-radix unsigned" "SHIFT" /testbench/uut/shift
+   add_wave_label "-radix ascii" "PLUS" /testbench/uut/plus_corrected
+   add_wave_label "-radix ascii" "MINUS" /testbench/uut/minus_corrected
+
+   add_wave_label "" "STATE" /testbench/uut/state
+   add_wave_label "" "NEXT_STATE" /testbench/uut/next_state
+   add_wave_label "" "FSM_MEALY_OUTPUT" /testbench/uut/fsm_mealy_output
 
    run 8 ns
 }
--- fpga/vigenere.vhd
+++ fpga/vigenere.vhd
@@ -15,27 +15,90 @@ entity vigenere is
     );
 end vigenere;
 
--- V souboru fpga/sim/tb.vhd naleznete testbench, do ktereho si doplnte
--- znaky vaseho loginu (velkymi pismeny) a znaky klice dle vaseho prijmeni.
-
 architecture behavioral of vigenere is
+    --Vigenere
+    signal shift: std_logic_vector(7 downto 0);
+    signal plus_corrected: std_logic_vector(7 downto 0);
+    signal minus_corrected: std_logic_vector(7 downto 0);
 
-    -- Sem doplnte definice vnitrnich signalu, prip. typu, pro vase reseni,
-    -- jejich nazvy doplnte tez pod nadpis Vigenere Inner Signals v souboru
-    -- fpga/sim/isim.tcl. Nezasahujte do souboru, ktere nejsou explicitne
-    -- v zadani urceny k modifikaci.
-
+    --Mealy FSM
+    type state_t is (ADD, SUB);
+    signal state: state_t := ADD;
+    signal next_state: state_t := SUB;
+    signal fsm_mealy_output: std_logic_vector(1 downto 0);
 
 begin
+    ---- Vigenere shifting ----
+    shift_process: process ( DATA, KEY )
+    begin
+        shift <= KEY - 64;
+    end process; -- shift_process
 
-    -- Sem doplnte popis obvodu. Doporuceni: pouzivejte zakladni obvodove prvky
-    -- (multiplexory, registry, dekodery,...), jejich funkce popisujte pomoci
-    -- procesu VHDL a propojeni techto prvku, tj. komunikaci mezi procesy,
-    -- realizujte pomoci vnitrnich signalu deklarovanych vyse.
+    plus_process : process( DATA, shift )
+        variable temp: std_logic_vector(7 downto 0);
+    begin
+        temp := DATA + shift;
+        if ( temp > 90 ) then
+            temp := temp - 26;
+        end if;
+        plus_corrected <= temp;
+    end process; -- plus_process
 
-    -- DODRZUJTE ZASADY PSANI SYNTETIZOVATELNEHO VHDL KODU OBVODOVYCH PRVKU,
-    -- JEZ JSOU PROBIRANY ZEJMENA NA UVODNICH CVICENI INP A SHRNUTY NA WEBU:
-    -- http://merlin.fit.vutbr.cz/FITkit/docs/navody/synth_templates.html.
+    minus_process : process( DATA, shift )
+        variable temp: std_logic_vector(7 downto 0);
+    begin
+        temp := DATA - shift;
+        if ( temp < 65 ) then
+            temp := temp + 26;
+        end if;
+        minus_corrected <= temp;
+    end process; -- minus_process
+    ---- End of Vigenere shifting ----
 
+
+    ---- Mealy finite state machine logic ----
+    state_logic : process( CLK, RST )
+    begin
+        if ( RST = '1' ) then
+            state <= ADD;
+        elsif rising_edge(CLK) then
+            state <= next_state;
+        end if;
+    end process; -- state_logic
+
+    fsm_mealy : process( state, DATA, RST )
+    begin
+        case( state ) is
+            when ADD =>
+                next_state <= SUB;
+                fsm_mealy_output <= "01"; --add
+        
+            when SUB =>
+                next_state <= ADD;
+                fsm_mealy_output <= "10"; --sub
+        end case;
+
+        if ( RST = '1' or ( DATA > 47 and DATA < 58 )) then
+            fsm_mealy_output <= "11";
+        end if;
+    end process; -- fsm_mealy
+    ---- End of Mealy finite state machine logic ----
+
+
+    ---- Multiplexer ----
+    mux : process( fsm_mealy_output, plus_corrected, minus_corrected )
+    begin
+        case( fsm_mealy_output ) is
+            when "01" =>
+                CODE <= plus_corrected;
+            
+            when "10" =>
+                CODE <= minus_corrected;
+
+            when others =>
+                CODE <= "00100011"; -- '#'
+        end case;
+    end process; -- mux
+    ---- End of Multiplexer ----
 
 end behavioral;
